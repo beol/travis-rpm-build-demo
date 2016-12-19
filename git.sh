50c40694e4ef4021dabf4cecd4eeeca8ecec05b0
Leo Laksmana <beol@laksmana.com>
2016-12-19 22:47:07 +0800

    Add missing perl-ExtUtils-MakeMaker

--- git.sh
+++ git.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yum -y install rpm-build zlib-devel openssl-devel curl-devel expat-deve gettext gcc
+yum -y install rpm-build zlib-devel openssl-devel curl-devel expat-devel gettext gcc perl-ExtUtils-MakeMaker
 
 curl -L https://www.kernel.org/pub/software/scm/git/git-2.11.0.tar.gz | tar xzf -
 
