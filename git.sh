bdac940ed5291513202c3b6f765c1aef40bc19bb
Leo Laksmana <beol@laksmana.com>
2016-12-19 23:01:13 +0800

    Trim build options

--- git.sh
+++ git.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-yum -y install rpm-build zlib-devel openssl-devel curl-devel expat-devel gettext gcc perl-ExtUtils-MakeMaker
+yum -y install gcc rpm-build zlib-devel openssl-devel curl-devel perl-ExtUtils-MakeMaker
 
 curl -L https://www.kernel.org/pub/software/scm/git/git-2.11.0.tar.gz | tar xzf -
 
-cd git-2.11.0
-
-make configure && ./configure && make all
+cd git-2.11.0 && make configure && ./configure && make all NO_EXPAT=YesPlease NO_TCLTK=YesPlease NO_GETTEXT=YesPlease
