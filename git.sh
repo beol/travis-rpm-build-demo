4734cb966fb08a710829dcbbca45eb011410799a
Leo Laksmana <beol@laksmana.com>
2016-12-19 22:40:03 +0800

    Missing gcc package

--- git.sh
+++ git.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/bash -e -v
 
-yum -y install rpm-build zlib-devel openssl-devel curl-devel expat-deve gettext
+yum -y install rpm-build zlib-devel openssl-devel curl-devel expat-deve gettext gcc
 
 curl -L https://www.kernel.org/pub/software/scm/git/git-2.11.0.tar.gz | tar xzf -
 
