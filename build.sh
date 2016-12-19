f52d6b1f7c093166d84c1827220c6e6a05ad4e2c
Leo Laksmana <beol@laksmana.com>
2016-12-20 07:45:36 +0800

    Define topdir for rpm build

--- build.sh
+++ build.sh
@@ -7,4 +7,8 @@ yum -y install gcc \
     curl-devel \
     perl-ExtUtils-MakeMaker
 
-rpmbuild -bc -vv /code/git.spec
+mkdir -p /code/rpmbuild/{SPECS,SOURCES,BUILD,RPMS,SRPMS,BUILDROOT}
+
+curl -s -L -o /code/rpmbuild/SOURCES/git-2.11.0.tar.gz http://kernel.org/pub/software/scm/git/git-2.11.0.tar.gz
+
+rpmbuild -bc -vv --define '_topdir /code/rpmbuild' /code/git.spec
