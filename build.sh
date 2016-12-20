a81b305e268dcda027325e4108101b2c3c8a41cf
Leo Laksmana <beol@laksmana.com>
2016-12-20 08:44:20 +0800

    Install package

--- build.sh
+++ build.sh
@@ -12,4 +12,4 @@ mkdir -p /code/rpmbuild/{SPECS,SOURCES,BUILD,RPMS,SRPMS,BUILDROOT}
 
 curl -s -L -o /code/rpmbuild/SOURCES/git-2.11.0.tar.gz http://kernel.org/pub/software/scm/git/git-2.11.0.tar.gz
 
-rpmbuild -bc -vv --define '_topdir /code/rpmbuild' /code/git.spec
+rpmbuild -bi -vv --define '_topdir /code/rpmbuild' /code/git.spec
