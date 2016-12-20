7ce9b849ac10f1ad8b5ca7d3ec794889e6d3aa00
Leo Laksmana <beol@laksmana.com>
2016-12-20 20:40:36 +0800

    Remove verbose

--- build.sh
+++ build.sh
@@ -14,4 +14,4 @@ mkdir -p $BASE_DIR/rpmbuild/{SPECS,SOURCES,BUILD,RPMS,SRPMS,BUILDROOT}
 
 curl -s -L -o $BASE_DIR/rpmbuild/SOURCES/git-2.11.0.tar.gz http://kernel.org/pub/software/scm/git/git-2.11.0.tar.gz
 
-rpmbuild -bb -vv --define "_topdir ${BASE_DIR}/rpmbuild" --without docs $BASE_DIR/git.spec
+rpmbuild -bb --define "_topdir ${BASE_DIR}/rpmbuild" --without docs $BASE_DIR/git.spec
