17e0410fe2ceb2fc97f3a7189498fa7e800dcd00
Leo Laksmana <beol@laksmana.com>
2016-12-29 13:58:37 +0800

    Update ci build
    Travis CI build script updated after changes made to RPM spec file.

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
+#!/bin/env bash
 
 BASE_DIR=$(dirname $0)
 
-VERSION=$(grep Version ${BASE_DIR}/git.spec | awk '{print $2}')
+VERSION='2.11.0'
+_VERSION=$(echo $VERSION | cut -d. -f1,2 | sed s/\\.//)
 
 yum -y install gcc \
        rpm-build \
@@ -14,10 +15,14 @@ yum -y install gcc \
 
 mkdir -p $BASE_DIR/rpmbuild/{SPECS,SOURCES,BUILD,RPMS,SRPMS,BUILDROOT}
 
-curl -s -L -o $BASE_DIR/rpmbuild/SOURCES/git-$VERSION.tar.gz http://kernel.org/pub/software/scm/git/git-$VERSION.tar.gz
+curl -s -L -o $BASE_DIR/rpmbuild/SOURCES/git-$VERSION.tar.gz \
+    http://kernel.org/pub/software/scm/git/git-$VERSION.tar.gz
 
-rpmbuild -bb --define 'debug_package %{nil}' --define "_topdir ${BASE_DIR}/rpmbuild" --without docs $BASE_DIR/git.spec
+rpmbuild -bb \
+         --define 'debug_package %{nil}' \
+         --define "_topdir ${BASE_DIR}/rpmbuild" \
+         $BASE_DIR/git.spec
 
-yum localinstall -y $BASE_DIR/rpmbuild/RPMS/x86_64/*.rpm
+yum localinstall -y $BASE_DIR/rpmbuild/RPMS/x86_64/git${_VERSION}-$VERSION-*.rpm
 
-/opt/git/bin/git --version
+/opt/git${_VERSION}/bin/git --version
