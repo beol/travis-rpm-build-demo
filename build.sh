36336f476b88d5fdaa271adf33a212da5a6539ca
Leo <beol@laksmana.com>
2017-01-19 22:41:16 +0000

    Update Travis build to handle tags

--- build.sh
+++ build.sh
@@ -3,11 +3,13 @@
 set -ev
 
 BASE_DIR=$(dirname $0)
+VERSION=${1:-2.11.0}
+RELEASE=${2:-0a}
 
 cd $BASE_DIR
 
 spectool -g -R git.spec
-rpmbuild -bb git.spec
+rpmbuild -bb --define "_version ${VERSION}" --define "_release ${RELEASE}" git.spec
 
 [[ -n "${GPG_PASSPHRASE}" ]] && find ./rpmbuild/RPMS -type f -name "*.rpm" | \
         xargs -I{} sh -c "./rpm-sign.exp {} && rpm --checksig {}"
