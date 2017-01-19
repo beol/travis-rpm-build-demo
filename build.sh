32c746331bada5888574e8d2c409be6eb610298b
Leo <beol@laksmana.com>
2017-01-19 23:02:11 +0000

    Fix build script

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ RELEASE=${2:-0a}
 
 cd $BASE_DIR
 
-spectool -g -R git.spec
+spectool -g -R --define "_version ${VERSION}" --define "_release ${RELEASE}" git.spec
 rpmbuild -bb --define "_version ${VERSION}" --define "_release ${RELEASE}" git.spec
 
 [[ -n "${GPG_PASSPHRASE}" ]] && find ./rpmbuild/RPMS -type f -name "*.rpm" | \
